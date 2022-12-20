afd5cd593774573b60cad931d93214375e8d1ca1
nguyenduchao95 <102244364+nguyenduchao95@users.noreply.github.com>
2022-12-20 22:52:35 +0700

    update 20/12/22

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -4,6 +4,7 @@ import HomeHeader from '../../HomePage/HomeHeader';
 import './DoctorDetail.scss';
 import { getDetailInfoDoctor } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils'
+import DoctorSchedule from './DoctorSchedule';
 
 
 class DetailDoctor extends Component {
@@ -58,6 +59,13 @@ class DetailDoctor extends Component {
                     </div>
 
                     <div className="schedule-doctor">
+                        <div className="content-left">
+                            <DoctorSchedule />
+                        </div>
+
+                        <div className="content-right">
+
+                        </div>
 
                     </div>
 
